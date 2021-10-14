42aa2cdfe185e1f8dfdc6356c0ae06e32b62af00
tobychadderton1 <hapaxemail@mail.com>
2021-10-14 22:26:28 +0100

    added basic fire POST request

--- src/ws4server.py
+++ src/ws4server.py
@@ -1,1 +1,27 @@
-import ws4controls
+from ws4controls import Ws4dummy
+
+from flask import Flask
+
+app = Flask(__name__)
+ws4 = Ws4dummy()
+
+@app.route("/")
+def home():
+    return """
+<script>
+function sendfire() {
+    let request = new XMLHttpRequest();
+    request.open("POST", "fire", true);
+    request.send()
+}
+</script>
+
+<button onclick="sendfire()">Fire</button>
+"""
+
+@app.route("/fire", methods = ["POST"])
+def fire():
+    ws4.fire()
+    return "Fired"
+
+app.run(host="0.0.0.0")
