edb94af5563e47d006c234661d2ee16775de6ac5
tobychadderton1 <hapaxemail@mail.com>
2021-10-17 02:15:53 +0100

    improved code formatting

--- src/ws4server.py
+++ src/ws4server.py
@@ -1,32 +1,32 @@
-import ws4video
-import ws4controls
+import ws4video as video
+import ws4controls as controls
 
 from flask import Flask, render_template, Response
 
-app = Flask(__name__)
-ws4inter = ws4controls.Ws4dummy()
+flaskapp = Flask(__name__)
+ws4inter = controls.Ws4dummy()
 
-@app.route("/", methods=["GET"])
+@flaskapp.route("/", methods=["GET"])
 def home():
     return render_template("index.html")
 
-@app.route("/video_feed")
+@flaskapp.route("/video_feed")
 def video_feed():
-    return Response(ws4video.gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(video.gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route("/fire", methods = ["POST"])
+@flaskapp.route("/fire", methods = ["POST"])
 def fire():
     ws4inter.fire()
     return "Fired"
 
-@app.route("/left", methods = ["POST"])
+@flaskapp.route("/left", methods = ["POST"])
 def left():
     ws4inter.move_left()
     return "Turned left"
 
-@app.route("/right", methods = ["POST"])
+@flaskapp.route("/right", methods = ["POST"])
 def right():
     ws4inter.move_right()
     return "Turned right"
 
-app.run(host="0.0.0.0")
+flaskapp.run(host="0.0.0.0")
--- src/ws4video.py
+++ src/ws4video.py
@@ -1,13 +1,14 @@
-import cv2
+import cv2 as opencv
 
-camera = cv2.VideoCapture(0)
+camera = opencv.VideoCapture(0)
 
 def gen_frames():
+    """Generator function that continuously yields frames from a webcam connected to the client device"""
     while True:
         success, frame = camera.read()
         if not success:
             break
         else:
-            ret, buffer = cv2.imencode('.jpg', frame)
+            ret, buffer = opencv.imencode('.jpg', frame)
             frame = buffer.tobytes()
             yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
